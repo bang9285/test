4c79f4ed13d9716bc42f4020bd4ddbd919560928
BANG <BANG>
2019-05-25 14:24:54 +0900

    A.java Commit

--- A.java
+++ A.java
@@ -5,5 +5,12 @@ public class A {
 
     public static void main(String[] args) {
         System.out.println("A.java");
+
+        System.out.println("A.java");
+        System.out.println("A.java");
+        System.out.println("A.java");
+        System.out.println("A.java");
+        System.out.println("A.java");
+        System.out.println("A.java");
     }
 }
